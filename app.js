54c10cf777fa8c0f77fba209f7ccd9358c8fcec1
Lalit-7881 <lalitsonawane391@gmail.com>
2023-05-07 00:59:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+  const button = document.querySelector("#btn");
+  button.addEventListener("click", calculateBmi);
+};
+
+function calculateBmi() {
+  const height = document.querySelector("#height").value;
+  const weight = document.querySelector("#weight").value;
+  const result = document.querySelector("#result");
+
+  if (!height || isNaN(height) || height < 0) {
+    result.innerText = "Please provide a valid height";
+    return;
+  } else if (!weight || isNaN(weight) || weight < 0) {
+    result.innerText = "Please provide a valid weight";
+    return;
+  }
+
+  const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+  if (bmi < 18.5) {
+    result.innerText = `Under Weight: ${bmi}`;
+  } else if (bmi >= 18.5 && bmi < 24.9) {
+    result.innerText = `Normal: ${bmi}`;
+  } else if (bmi >= 25 && bmi < 29.9) {
+    result.innerText = `Over Weight: ${bmi}`;
+  } else if (bmi >= 30 && bmi < 34.9) {
+    result.innerText = `Obesity (Class I): ${bmi}`;
+  } else if (bmi >= 35.5 && bmi < 39.9) {
+    result.innerText = `Obesity (Class II) : ${bmi}`;
+  } else {
+    result.innerText = `Extreme Obesity: ${bmi}`;
+  }
+}
